7b449f57c31b5ebac9e0919481d1e1a1eb6392b8
Anish <anishbasoup@gmail.com>
2025-03-10 11:26:44 +1100

    Update init.lua

--- init.lua
+++ init.lua
@@ -818,7 +818,7 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'cocopon/iceberg',
+    'cocopon/iceberg.vim',
     name = 'iceberg',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
